9de13f90b69832dbd4aa0933860d84af86854c12
shashimal2000 <115067658+shashimal2000@users.noreply.github.com>
2024-04-11 14:54:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,10 +16,10 @@ Creator : @darkmakerofc , @mr-nima-x
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "94788779949",
+  SUDO_NUMBERS : "94788779949,94788779949",
+  OWNER_NAME: "『 SHASHI 』",
+  BOT_NAME : "SHASHIMAL",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
